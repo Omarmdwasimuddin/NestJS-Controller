7aa8405547c20db7091e1d27a44a645502b44ed1
Md Wasim Uddin <mdwasimu015@gmail.com>
2026-02-24 08:44:39 +0600

    nestjs controller

---
+++ src/cats/cats.controller.ts
@@ -0,0 +1,11 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('cats')
+export class CatsController {
+
+    @Get('breed')
+    findAll(): string {
+        return 'This action returns all cats';
+    }
+
+}
